6692a0d11757560c354f3676cba6fe311f2370a8
Abdul <Abdul@Abdul-PC.hitronhub.home>
2017-09-05 21:39:18 -0600

    Added a lot of shit

--- main/main/model/players/AchievementHandler.java
+++ main/main/model/players/AchievementHandler.java
@@ -9,6 +9,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import main.util.DatabaseConnection;
 
 /**
@@ -21,11 +24,16 @@ public class AchievementHandler {
     private HashMap<Integer, Integer> completed = new HashMap<Integer, Integer>();
     Player player;
 
-    public void loadProgressAchievements(Player player) {
+    public AchievementHandler(Player a) {
+        this.player = a;
+    }
+
+    public void loadProgressAchievements() {
         java.sql.Connection con = DatabaseConnection.getConnection();
         PreparedStatement ps;
         try {
             ps = con.prepareStatement("select achid,progress from achievements where state = 0 and charid = ?");
+            ps.setString(1, player.playerName);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 progress.put(rs.getInt("achid"), rs.getInt("progress"));
@@ -37,11 +45,12 @@ public class AchievementHandler {
         }
     }
 
-    public void loadCompletedAchievements(Player player) {
+    public void loadCompletedAchievements() {
         java.sql.Connection con = DatabaseConnection.getConnection();
         PreparedStatement ps;
         try {
             ps = con.prepareStatement("select achid,progress from achievements where state = 1 and charid = ?");
+            ps.setString(1, player.playerName);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 completed.put(rs.getInt("achid"), rs.getInt("progress"));
@@ -53,12 +62,65 @@ public class AchievementHandler {
         }
     }
 
-    public void loadAllAchievements(Player player) {
-        loadCompletedAchievements(player);
-        loadProgressAchievements(player);
+    public void loadAllAchievements() {
+        loadCompletedAchievements();
+        loadProgressAchievements();
     }
 
-    public void saveProgressAchievements(Player player) {
-
+    public void saveProgressAchievements() {
+        java.sql.Connection con = DatabaseConnection.getConnection();
+        PreparedStatement ps;
+        Set<Map.Entry<Integer, Integer>> entrySet = progress.entrySet();
+        try {
+            ps = con.prepareStatement("delete * from achievements where charid = ? and state = 0");
+            ps.setString(1, player.playerName);
+            ps.executeQuery();
+            for (Entry entry : entrySet) {
+                ps = con.prepareStatement("insert into achievements (charid,achid,progress,state) values (?,?,?,?)");
+                ps.setString(1, player.playerName);
+                ps.setInt(2, (int) entry.getKey());
+                ps.setInt(3, (int) entry.getValue());
+                ps.setInt(4, 0);
+                ps.executeQuery();
+            }
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
+    public void saveCompletedAchievements() {
+        java.sql.Connection con = DatabaseConnection.getConnection();
+        PreparedStatement ps;
+        Set<Map.Entry<Integer, Integer>> entrySet = completed.entrySet();
+        try {
+            ps = con.prepareStatement("delete * from achievements where charid = ? and state = 1");
+            ps.setString(1, player.playerName);
+            ps.executeQuery();
+            for (Entry entry : entrySet) {
+                ps = con.prepareStatement("insert into achievements (charid,achid,progress,state) values (?,?,?,?)");
+                ps.setString(1, player.playerName);
+                ps.setInt(2, (int) entry.getKey());
+                ps.setInt(3, (int) entry.getValue());
+                ps.setInt(4, 1);
+                ps.executeQuery();
+            }
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void completeAchievement(int achid) {
+        java.sql.Connection con = DatabaseConnection.getConnection();
+        PreparedStatement ps;
+        try {
+            ps = con.prepareStatement("delete * from achievements where charid = ? and achid = " + achid);
+            ps.setString(1, player.playerName);
+            ps.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
